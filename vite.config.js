5778a037eb112e650b560b31322215b22eaa8a87
KalpickSharma <128212271+KalpickSharma@users.noreply.github.com>
2025-08-31 13:39:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const vite_1 = require("vite");
-const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
-// https://vitejs.dev/config/
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [(0, plugin_react_1.default)()],
-    server: {
-        host: true, // This will expose the server to your network
-        port: 3000 // Optional: specify a port
-    }
-});
+  plugins: [react()],
+  server: {
+    host: true, // expose server
+    port: 3000, // optional
+  },
+})
