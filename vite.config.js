e60829c6d87dc8fe1e92d3ea4cef15cb8161299f
KalpickSharma <128212271+KalpickSharma@users.noreply.github.com>
2025-08-31 12:29:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vite_1 = require("vite");
 const plugin_react_1 = __importDefault(require("@vitejs/plugin-react"));
 // https://vitejs.dev/config/
-exports.default = (0, vite_1.defineConfig)({
+import { defineConfig } from 'vite'
+
+export default defineConfig({
     plugins: [(0, plugin_react_1.default)()],
     server: {
         host: true, // This will expose the server to your network
